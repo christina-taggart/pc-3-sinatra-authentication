6016b14c6edbee1d3f637305c7407e51ef72fafb
Anne Spalding <aespaldi@gmail.com>
2014-01-31 15:37:57 -0800

    add authentication directory

---
+++ source/authenication/db/seeds.rb
@@ -0,0 +1,11 @@
+require 'faker'
+
+# create a few users
+
+#TODO: Once you have implemented BCrypt - you can use these to seed your database.
+
+
+# User.create :name => 'Dev Bootcamp Student', :email => 'me@example.com', :password => 'password'
+# 5.times do
+#   User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password => 'password'
+# end
