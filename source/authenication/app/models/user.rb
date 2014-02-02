c61ba02e15d8f7f625b4baddbfb31332e2ee5faf
John Olmsted <johnaolmsted@gmail.com>
2014-02-02 07:04:04 -0800

    Adds models and migrations

--- source/authenication/app/models/user.rb
+++ source/authenication/app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  #TODO : Use bcrypt to store hashed passwords and authenticate users
+  validates :email, uniqueness: true
+  before_save :encrypt_email
+
+  def self.authenticate(email, password)
+    !User.where(email: email, password_hash: Digest::SHA256.hexdigest(password)).empty?
+  end
+
+  def encrypt_email
+    self.password_hash = Digest::SHA256.hexdigest(self.password_hash)
+  end
 end
--- source/authenication/db/migrate/20140127173922_create_users.rb
+++ source/authenication/db/migrate/20140127173922_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password_hash
+      t.string :access_level
  
       t.timestamps
     end
