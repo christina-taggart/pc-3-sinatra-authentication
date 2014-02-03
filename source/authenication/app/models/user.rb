af89929e7b6a8b0ff221b487d8f6f1ece253aeed
Patrick <Patrick@Patricks-MacBook-Air.local>
2014-02-02 17:06:39 -0800

    Release 0: Implement Sign In

--- source/authenication/app/models/user.rb
+++
@@ -1,3 +0,0 @@
-class User < ActiveRecord::Base
-  #TODO : Use bcrypt to store hashed passwords and authenticate users
-end
--- source/authenication/public/js/application.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-
-  // Optional - Use AJAX to send an HTTP DELETE request for the sign-out link
-  
-
-});
