ec47ad624b3d9c11ae0308bc3d9c069748df859d
Emily Foley & Emmanuel Kaunitz <pair+EmFoley.mechamanny@devbootcamp.com>
2014-02-03 21:02:32 -0800

    Improved error messaging using bullets and Rack::flash

--- source/authenication/app/models/user.rb
+++ source/authenication/app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
 	attr_accessor :pre_password, :password
 
-	validates :pre_password, presence: true, :length => {:minimum => 6}
+	validates :pre_password, presence: true, :length => {:minimum => 6, :message => "Password must be at least 6 digits"}
 
-	validates :email, presence: true, :format => { :with => /^.+@.+\..+$/, :message => "Not valid email address"}
+	validates_presence_of :email, :message => "You must enter an email"
+  validates :email, :format => { :with => /^.+@.+\..+$/, :message => "Not valid email address"}
+  validates_uniqueness_of :email, :message => "Email has already been used"
 
   after_validation :encrypt_password
 
