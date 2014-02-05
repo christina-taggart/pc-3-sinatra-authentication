9ed4a30b27914761f9d294721df7cc2ef8ce8b2e
Apprentice <apprentice@dbc18.local>
2014-02-04 23:49:21 -0800

    Fixed up some of the routes and added falsh error messages

--- source/authenication/app/models/user.rb
+++ source/authenication/app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
-  #TODO : Use bcrypt to store hashed passwords and authenticate users
+  validates_presence_of :name, :message=>"Umm, whats your name?"
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password_hash, presence: true
+  before_save :encrypt_password
+
+
+  def self.validate(params, password)
+    begin
+      user_password = User.find_by_email(params[:email]).password_hash
+      user_password == password
+    rescue
+      false
+    end
+  end
+
+  def encrypt_password
+    self.password_hash = Digest::SHA256.hexdigest(self.password_hash)
+  end
+
+
 end
