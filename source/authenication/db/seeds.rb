7c19dde9973c0dc8ef0153feed9657fb4671ce67
Emily Foley <foleyemilym@gmail.com>
2014-02-06 15:30:07 -0800

    Seeded db with Faker

--- source/authenication/db/seeds.rb
+++ source/authenication/db/seeds.rb
@@ -1,11 +1,6 @@
 require 'faker'
 
-# create a few users
 
-#TODO: Once you have implemented BCrypt - you can use these to seed your database.
-
-
-# User.create :name => 'Dev Bootcamp Student', :email => 'me@example.com', :password => 'password'
-# 5.times do
-#   User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password => 'password'
-# end
+5.times do
+  User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password_hash => BCrypt::Password.create(((Faker::Lorem).words),cost: 5)
+end
