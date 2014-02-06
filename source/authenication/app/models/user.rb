0ca5df48080cc655f1c15b97551bd85e4c1f5605
Ricardo Rubio <rickarubio@gmail.com>
2014-02-05 21:17:41 -0800

    Stubbed out User#hash_the_password

--- source/authenication/app/models/user.rb
+++ source/authenication/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   #TODO : Use bcrypt to store hashed passwords and authenticate users
+  before_validation :hash_the_password
+
+  def hash_the_password
+    # hash user password
+  end
 end
