30f3d759b56740b2c9a0be3102009cea9ef4eef8
Ricardo Rubio <rickarubio@gmail.com>
2014-02-06 12:19:28 -0800

    Fixed faulty implementation of User#encrypt_password and implemented User#authenticate_user(password)

--- source/authenication/app/models/user.rb
+++ source/authenication/app/models/user.rb
@@ -2,7 +2,11 @@ class User < ActiveRecord::Base
   #TODO : Use bcrypt to store hashed passwords and authenticate users
   before_validation :encrypt_password
 
-  def encrypt_password(password)
-    BCrypt::Password.create(password)
+  def encrypt_password
+    self.password_hash = BCrypt::Password.create(password_hash)
+  end
+
+  def authenticate_user(password)
+    BCrypt::Password.new(self.password_hash) == password
   end
 end
