b6652ee1c22bf18bb3f1e06ae0f92643cf378119
Apprentice <apprentice@dbc15.local>
2014-02-06 18:16:39 -0800

    User can create, log in, log out, and see list of all users. Weird css bug moves 'User Authentication' text.

--- source/authenication/app/controllers/index.rb
+++ source/authenication/app/controllers/index.rb
@@ -1,3 +1,6 @@
+enable :sessions
+require 'bcrypt'
+
 get '/' do
   # render home page
  #TODO: Show all users if user is signed in
@@ -7,23 +10,44 @@ end
 #----------- SESSIONS -----------
 
 get '/sessions/new' do
-  # render sign-in page 
+  # render sign-in page
+  erb :sign_in
 end
 
 post '/sessions' do
   # sign-in
+  @user = User.find_by_email(params[:email])
+  if @user.password == params[:password] #check with the encryption
+    session[:id] = @user.id
+    redirect '/'
+  else
+    print "wrong password"
+    p params
+  end
 end
 
 delete '/sessions/:id' do
-  # sign-out -- invoked 
+  # sign-out -- invoked
+  session[:id] = nil
+  redirect '/'
 end
 
 #----------- USERS -----------
 
 get '/users/new' do
   # render sign-up page
+  erb :sign_up
 end
 
 post '/users' do
   # sign-up a new user
+  @user = User.new(params[:user])
+  @user.password = params[:user][:password]
+  @user.save!
+  redirect '/'
 end
+
+
+
+
+
--- source/authenication/app/helpers/sessions.rb
+++ source/authenication/app/helpers/sessions.rb
@@ -2,6 +2,8 @@ helpers do
 
   def current_user
         # TODO: return the current user if there is a user signed in.
+    session[:id] ? @logged_in = true : @logged_in = false
+    @user = User.find(session[:id]) if @logged_in
   end
 
 end
