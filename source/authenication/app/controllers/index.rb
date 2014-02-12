99b37937e2ced31f7523b530ee785c5e97c7011a
Emily Foley <foleyemilym@gmail.com>
2014-02-11 23:58:22 -0800

    refactoring sessions in progress.

--- source/authenication/app/controllers/index.rb
+++ source/authenication/app/controllers/index.rb
@@ -1,20 +1,27 @@
-get '/' do
-  @users= User.all
-  erb :index
-end
 
 #----------- SESSIONS -----------
 
-get '/sessions/new' do
+enable :sessions
+
+get '/' do
+ @users = User.all
+ erb :index
+end
+
+get '/sessions/new' do 
   erb :sign_in
 end
 
-post '/sessions' do
+post '/sessions' do 
+  @user = User.find_by_email(params[:email])
+  if session[:id] = @user.id 
+     session[:logged_in] = true
   redirect '/'
-end
+  end
 
 delete '/sessions/:id' do
-  # sign-out -- invoked
+  session[:id] = nil
+  redirect '/'
 end
 
 #----------- USERS -----------
@@ -23,7 +30,10 @@ get '/users/new' do
   erb :sign_up
 end
 
+
 post '/users' do
-  User.create(name: params[:name], email: params[:email], password_hash: BCrypt::Password.create(params[:password],cost: 5))
-  erb :sign_in
+  @user = User.create(name: params[:name], email: params[:email], password_hash: BCrypt::Password.create(params[:password],cost: 5))
+  session[:id] = @user.id
+  redirect '/'
+end
 end
--- source/authenication/app/helpers/sessions.rb
+++ source/authenication/app/helpers/sessions.rb
@@ -1,7 +1,9 @@
 helpers do
 
   def current_user
-        # TODO: return the current user if there is a user signed in.
+    if session[:id]
+      User.find(session[:id]) 
+    end
   end
 
 end
